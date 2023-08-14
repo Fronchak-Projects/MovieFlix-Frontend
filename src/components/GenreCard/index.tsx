b82afc67170d3b2719f0c7b847918d0214d80511
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-08-13 22:22:02 -0300

    Implement method to delete genre

--- src/components/GenreCard/index.tsx
+++ src/components/GenreCard/index.tsx
@@ -5,9 +5,10 @@ import { BASE_API_URL } from "../../utils/Contantes";
 
 type Props = {
   genre: GenreType;
+  handleDelete: (id: number) => void
 }
 
-const GenreCard = ({ genre }: Props) => {
+const GenreCard = ({ genre, handleDelete }: Props) => {
   return (
     <div className="bg-white rounded-lg h-full flex flex-col">
       <img
@@ -18,7 +19,7 @@ const GenreCard = ({ genre }: Props) => {
         <h3 className="text-xl mb-2"><Link className="hover:underline hover:text-blue-400" to={`/genres/${genre.id}`}>{ genre.name }</Link></h3>
         <div className="flex justify-end gap-x-2">
           <Link to={`/genres/edit/${genre.id}`} className="btn px-4 py-1 bg-blue-500 hover:bg-blue-700">Editar</Link>
-          <button className="btn px-4 py-1 bg-red-500 hover:bg-red-700">Excluir</button>
+          <button className="btn px-4 py-1 bg-red-500 hover:bg-red-700" onClick={() => handleDelete(genre.id)}>Excluir</button>
         </div>
       </div>
     </div>
--- src/pages/Genres/index.tsx
+++ src/pages/Genres/index.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import { BASE_API_URL } from '../../utils/Contantes';
 import GenreType from '../../types/models/GenreType';
 import GenreCard from '../../components/GenreCard';
+import useFetchFunction from '../../hooks/useFetchFunction';
+import useAuth from '../../hooks/useAuth';
+import { toast } from "react-toastify";
 
 const Genres = () => {
 
@@ -12,7 +15,11 @@ const Genres = () => {
       "Accept": "application/json"
     }
   });
+  const { isAuthenticated, hasAnyRole, logout, token } = useAuth();
   const [genres, setGenres] = useState<Array<GenreType> | undefined>();
+  const [idDeleted, setIdDeleted] = useState<number | undefined>();
+  const useFetchFunctionObj = useFetchFunction();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if(data !== undefined) {
@@ -20,11 +27,40 @@ const Genres = () => {
     }
   }, [data]);
 
+  const handleDelete = (id: number) => {
+    if(!isAuthenticated()) {
+      navigate("/auth/login");
+      logout();
+      toast.info("Você precisa estar logado para realizar essa ação");
+      return;
+    }
+    if(!hasAnyRole(['admin'])) {
+      toast.info("Você não tem permissão para realizar essa ação");
+      return;
+    }
+    setIdDeleted(id);
+    useFetchFunctionObj.fetchFunction(`${BASE_API_URL}/api/genres/${id}`, {
+      method: 'DELETE',
+      headers: {
+        "Accept": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    });
+  }
+
+  useEffect(() => {
+    if(useFetchFunctionObj.status === 204) {
+      toast.success("Gênero deletado com sucesso");
+      const nextGenres = genres?.filter((genre) => genre.id != idDeleted);
+      setGenres(nextGenres);
+    }
+  }, [useFetchFunctionObj.status, idDeleted])
+
   return (
     <div className="container px-2 mx-auto my-10">
       <div className="flex justify-between items-center mb-3">
         <h2 className="text-white font-bold text-2xl uppercase">Gêneros</h2>
-        <NavLink className="btn px-4 py-1 bg-blue-500 hover:bg-blue-700" to="/genres/save">Adicionar gênero</NavLink>
+        <Link className="btn px-4 py-1 bg-blue-500 hover:bg-blue-700" to="/genres/save">Adicionar gênero</Link>
       </div>
       { isLoading && isLoading === true && (
         <p className="text-xl">Carregando gêneros</p>
@@ -33,7 +69,7 @@ const Genres = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           { genres.map((genre) => (
             <div className="p-2" key={genre.id}>
-              <GenreCard genre={genre} />
+              <GenreCard genre={genre} handleDelete={handleDelete} />
             </div>
           )) }
         </div>
